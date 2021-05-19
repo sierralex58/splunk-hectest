e47e30244e2301b6d4778d4e1821206ade4a8163
tkmax <alexandre.masson@six-group.com>
2021-05-19 13:42:09 +0200

    added shell script fixed

--- src/main/java/fill.sh
+++ src/main/java/fill.sh
@@ -1,3 +1,3 @@
 #!/bin/csh
 
-java -jar -Dhectest.propertyfile=../resources/HECTest.properties -Dlog4j.configurationFile=../resources/log4j2.priv.xml ../../../target/HECTest-1.0-SNAPSHOT-jar-with-dependencies.jar 
+java -jar -Dhectest.propertyfile=../resources/HECTest.properties -Dlog4j.configurationFile=../resources/log4j2.xml ../../../target/HECTest-1.0-SNAPSHOT-jar-with-dependencies.jar
