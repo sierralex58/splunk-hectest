31fc5cd47fe69ab33668a04d553be38f4993dd9e
tkmax <alexandre.masson@six-group.com>
2021-05-19 11:05:26 +0200

    added final wait

--- src/main/java/Fill.java
+++ src/main/java/Fill.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Fill {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         FillIt fillit = new FillIt();
 
         fillit.doIt();
--- src/main/java/FillIt.java
+++ src/main/java/FillIt.java
@@ -15,49 +15,75 @@ public class FillIt {
     private int bytes;
     private int messages;
     private boolean pregenerate = false;
+    private long wait = 0;
+    private long finalwait = 0;
 
     public FillIt() throws IOException {
         log = LogManager.getLogger(this.getClass().getName());
         properties = new Properties();
         properties.load(new FileInputStream(System.getProperty("hectest.propertyfile")));
 
-        if(properties.getProperty("hectest.messages") == null || properties.getProperty("hectest.messages") == "0") {
+        if(properties.getProperty("hectest.messages") == null || properties.getProperty("hectest.messages").equals("0")) {
             messages = 100;
         } else {
             messages = Integer.parseInt(properties.getProperty("hectest.messages"));
         }
 
-        if(properties.getProperty("hectest.bytes") == null || properties.getProperty("hectest.bytes") == "0") {
+        if(properties.getProperty("hectest.bytes") == null || properties.getProperty("hectest.bytes").equals("0")) {
             bytes = 100;
         } else {
             bytes = Integer.parseInt(properties.getProperty("hectest.bytes"));
         }
 
-        if(properties.getProperty("hectest.pregenerate") == null || properties.getProperty("hectest.pregenerate") == "true") {
+        if(properties.getProperty("hectest.pregenerate") == null || properties.getProperty("hectest.pregenerate").equals("true")) {
             pregenerate = true;
+        }
+
+        if(properties.getProperty("hectest.wait") == null || properties.getProperty("hectest.wait").equals("0")) {
+            wait = 0;
+        } else {
+            wait = Integer.parseInt(properties.getProperty("hectest.wait"));
+        }
+        if(properties.getProperty("hectest.finalwait") == null || properties.getProperty("hectest.finalwait").equals("0")) {
+            finalwait = 0;
+        } else {
+            finalwait = Integer.parseInt(properties.getProperty("hectest.finalwait"));
         }
     }
 
-    public void doIt() {
+    public void doIt() throws InterruptedException {
         ArrayList<String> arrayList = new ArrayList<>();
+        int i = 0;
+
 
 
         if(!pregenerate) {
-            log.info("writing " + messages + " messages " + " with " + bytes + " bytes");
+            log.info(String.format("[%06d] writing %d messages with %d bytes", i++, messages, bytes));
 
-            for(int i = 0; i < messages; i++) {
+            for(; i < messages; i++) {
                 log.info(String.format("[%06d] %s", i, getAlphaNumericString(bytes)));
+                if(wait > 0) {
+                    Thread.sleep(wait);
+                }
             }
         } else {
-            log.info("generating " + messages + " messages with " + bytes + " bytes");
-            for(int i = 0; i < messages; i++) {
-                arrayList.add(String.format("[%06d] %s", i, getAlphaNumericString(bytes)));
+            log.info(String.format("[%06d] generating %d messages with %d bytes", i++, messages, bytes));
+            for(int count = 0; count < messages; count++) {
+                arrayList.add(String.format("[%06d] %s", count, getAlphaNumericString(bytes)));
             }
 
-            log.info("writing " + messages + " pre-generated messages");
-            for(int i = 0; i < messages; i++) {
+            log.info(String.format("[%06d] writing %d pre-generated messages with %d bytes", i++, messages, bytes));
+
+            for(; i < messages; i++) {
                 log.info(arrayList.get(i));
+                if(wait > 0) {
+                    Thread.sleep(wait);
+                }
             }
+        }
+
+        if(finalwait > 0) {
+            Thread.sleep(finalwait);
         }
 
 
